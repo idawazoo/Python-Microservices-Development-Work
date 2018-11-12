5269f3c0fa04ceb02fe6183f17805b7e7b12ab4c
ida wazoo <idawazoo@gmail.com>
2018-11-12 08:23:04 -0500

    functional test

---
+++ test_1.py
@@ -0,0 +1,15 @@
+import unittest
+import json
+from first_flask_8 import app as test_app
+
+class TestApp(unittest.TestCase):
+    def test_help(self):
+        app = test_app.test_client()
+
+        hello = app.get('/api')
+
+        body = json.loads(str(hello.data, 'utf8'))
+        self.assertEqual(body['Hello'], 'World!')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_2.py
@@ -0,0 +1,33 @@
+import unittest
+import json
+from error_handle_2 import app as test_app
+
+_404 = ('philtest 404 error.')
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = test_app.test_client()
+
+    def test_raise(self):
+        hello = self.app.get('/api')
+        print("-> {0}".format(hello.data))
+        body = json.loads(str(hello.data, 'utf8'))
+        print("--> {0}".format(body))
+        self.assertEqual(body['code'], 500)
+
+    def test_proper_404(self):
+        hello = self.app.get('/ajdlka')
+
+        self.assertEqual(hello.status_code, 404)
+
+        print("*> {0}".format(hello.data))
+
+        body = json.loads(str(hello.data, 'utf-8'))
+
+
+        self.assertEqual(body['code'], 404)
+        self.assertEqual(body['message'], '404: not Found')
+        self.assertEqual(body['description'], _404)
+
+if __name__ == '__main__':
+    unittest.main()
